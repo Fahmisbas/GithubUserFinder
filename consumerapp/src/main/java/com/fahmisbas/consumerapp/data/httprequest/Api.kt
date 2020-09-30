05f91a28a5cb647916bdd4d2e1ebad5c999debf6
Fahmi <fahmisulaimanbas@gmail.com>
2020-10-01 00:29:23 +0700

    edit manifest file

--- consumerapp/src/main/java/com/fahmisbas/consumerapp/data/httprequest/Api.kt
+++ consumerapp/src/main/java/com/fahmisbas/consumerapp/data/httprequest/Api.kt
@@ -2,6 +2,10 @@
  * Copyright (c) 2020 by Fahmi Sulaiman Baswedan
  */
 
+/*
+ * Copyright (c) 2020 by Fahmi Sulaiman Baswedan
+ */
+
 package com.fahmisbas.consumerapp.data.httprequest
 
 import com.fahmisbas.consumerapp.data.entities.UserData
@@ -15,20 +19,21 @@ import retrofit2.http.Query
 interface Api {
 
 
+
     @GET("search/users?")
-    @Headers("Authorization: token bbc75e90acbc00adffa11bd57c79e9c2ec71d9ff")
+    @Headers("Authorization: token YOUR API KEY")
     fun getUser(@Query("q") query: String): Call<UserItems>
 
     @GET("users/{username}")
-    @Headers("Authorization: token bbc75e90acbc00adffa11bd57c79e9c2ec71d9ff")
+    @Headers("Authorization: token YOUR API KEY")
     fun getUserDetail(@Path("username") usernameId: String): Call<UserData>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token bbc75e90acbc00adffa11bd57c79e9c2ec71d9ff")
+    @Headers("Authorization: token YOUR API KEY")
     fun getUserFollowing(@Path("username") usernameId: String): Call<List<UserData>>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token bbc75e90acbc00adffa11bd57c79e9c2ec71d9ff")
+    @Headers("Authorization: token YOUR API KEY")
     fun getUserFollowers(@Path("username") usernameId: String): Call<List<UserData>>
 
 }
